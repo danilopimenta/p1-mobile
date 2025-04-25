584329658128e53f5e735a1001aff512c29ff508
Danilo Pimenta <danilopi@outlook.com>
2025-04-25 00:27:33 -0300

    feat(p1): cria componente principal e faz hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/mdc-light-deeppurple/theme.css';
+
+import 'primeflex/primeflex.min.css'
+import 'primeicons/primeicons.css'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
